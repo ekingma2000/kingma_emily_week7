2b0c2b43145fbb1410083793b276436047ada127
Kingma <e_kingma@ats-hr420-dm20.fc.ca>
2020-02-28 09:51:07 -0500

    sounds

--- js/main.js
+++ js/main.js
@@ -84,7 +84,20 @@
             }
 
             // create and play explosion sound
-            
+            let audio = document.createElement('audio');
+            audio.src = "audio/explosion.mp3";
+
+            // remove sound after it is done playing
+            audio.addEventListener("ended", function() {
+              document.body.removeChild(audio);
+
+            });
+
+            document.body.appendChild(audio);
+
+            audio.play();
+
+
           }
         });
 
@@ -123,7 +136,7 @@
       window.requestAnimationFrame(draw);
   }
 
-  
+
   function createBullet() {
     // create / draw a bullet and push it into the bullet array
     let newBullet = {
@@ -137,7 +150,19 @@
     bullets.push(newBullet);
 
     // create and play cheesy laser sound
-    
+    let audio = document.createElement('audio');
+    audio.src = "audio/laser.mp3";
+
+// remove sound after it is done playing
+    audio.addEventListener("ended", function() {
+      document.body.removeChild(audio);
+
+    });
+
+    document.body.appendChild(audio);
+
+    audio.play();
+
   }
 
   function movePlayer(e) {
